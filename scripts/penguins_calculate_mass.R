e142165ca2d2000481fa4236898a3de030c6d0f7
Stephanie Spielman <stephanie.spielman@ccdatalab.org>
2023-08-09 15:27:06 -0400

    Update script to do island+species group, and only 2008

--- scripts/penguins_calculate_mass.R
+++ scripts/penguins_calculate_mass.R
@@ -1,11 +1,12 @@
 library(palmerpenguins)
 
 # Define path to output file
-output_tsv <- file.path("results", "penguins_mass.tsv")
+output_tsv <- here::here("results", "penguins_mass.tsv")
 
 # Create data frame of mean mass for each species
 penguin_mass <- penguins |>
-  dplyr::group_by(species) |>
+  dplyr::filter(year == 2008) |>
+  dplyr::group_by(species, island) |>
   # ignore NAs when calculating mean
   dplyr::summarize(mean_mass = mean(body_mass_g, na.rm=TRUE))
 
