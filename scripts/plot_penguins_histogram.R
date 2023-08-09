53739e37aa3d5d105fe9e932cd02e3e36571a98e
Stephanie Spielman <stephanie.spielman@ccdatalab.org>
2023-08-09 13:22:36 -0400

    Add optparse to script with a default of 40 bins, and use the bins in the histogram

--- scripts/plot_penguins_histogram.R
+++ scripts/plot_penguins_histogram.R
@@ -1,9 +1,22 @@
 # Load libraries ---------
 library(ggplot2)
 library(palmerpenguins)
+library(optparse)
+
+# Load input options -------
+option_list <- list(
+  make_option(
+    opt_str = c("--bins"),
+    type = "integer",
+    default = 40,
+    help = "number of bins to use in the histogram"
+  )
+)
+opt <- parse_args(OptionParser(option_list = option_list))
+user_bins <- opt$bins
 
 # Make histogram of penguin bill depth --------
 ggplot(penguins) +
   aes(x = bill_depth_mm) +
-  geom_histogram() +
+  geom_histogram(bins = user_bins) +
   labs(x = "Penguin bill depth (mm)")
