d32f70a9bf69607d3feed3c30bac8aa72285b3b0
Stephanie Spielman <stephanie.spielman@ccdatalab.org>
2023-08-10 14:41:34 -0400

    Add utils.R with function to perform linear regression and return tidy fit

---
+++ scripts/utils.R
@@ -0,0 +1,16 @@
+run_tidy_regression <- function(df, x_var, y_var) {
+  
+  # build model formula from input strings
+  model_formula <- as.formula(
+    glue::glue("{y_var} ~ {x_var}")
+  )
+  
+  # perform regression using formula
+  model_fit <- lm(model_formula, data = df)
+  
+  # tidy the output
+  tidy_model_fit <- broom::tidy(model_fit)
+  
+  # return tidy output
+  return(tidy_model_fit)
+}
