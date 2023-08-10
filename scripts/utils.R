090c7c7ff2fc27b95b63af8a94ed26a6a40cfc32
Stephanie Spielman <stephanie.spielman@ccdatalab.org>
2023-08-10 15:13:06 -0400

    Add utils function to make scatterplot

--- scripts/utils.R
+++ scripts/utils.R
@@ -24,3 +24,35 @@ run_tidy_regression <- function(df, x_var, y_var) {
   # return tidy output
   return(tidy_model_fit)
 }
+
+
+
+#' Plot scatterplot associated with a regression model
+#'
+#' @param df Data frame containing variables to plot
+#' @param x_var The predictor variable plainly written (not a string)
+#' @param y_var The predictor variable plainly written (not a string)
+#' @param filename Filename to export figure to
+#'
+#' @examples
+#'  plot_regression_scatterplot(iris, Sepal.Length, Sepal.Width, "sepal_model.png")
+#'
+#' @return Returns plot object, while also exporting to file
+plot_regression_scatterplot <- function(df,
+                                        x_var,
+                                        y_var,
+                                        filename) {
+  
+  # Create basic scatterplot
+  scatterplot <- ggplot(df) +
+    aes(x = {{x_var}}, y = {{y_var}}) +
+    geom_point() +
+    # Add trendline
+    geom_smooth(method = "lm")
+  
+  # Export to file at default size
+  ggsave(filename, scatterplot)
+  
+  # Return plot object
+  return(scatterplot)
+}
