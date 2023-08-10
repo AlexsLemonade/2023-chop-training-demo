0846d2b5dceb618fad5e4a2352613bf17be09ecf
Stephanie Spielman <stephanie.spielman@ccdatalab.org>
2023-08-10 15:18:12 -0400

    include scatterplot running in script

--- scripts/model_penguins.R
+++ scripts/model_penguins.R
@@ -5,8 +5,9 @@ source(
   here::here("scripts", "utils.R")
 )
 
-# define output file
+# define output files
 output_model_fit <- here::here("results", "penguin_model_fit.tsv")
+output_model_figure <- here::here("plots", "penguin_model_scatterplot.png")
 
 # run model
 penguin_model <- run_tidy_regression(
@@ -17,3 +18,14 @@ penguin_model <- run_tidy_regression(
 
 # export model
 write_tsv(penguin_model, output_model_fit)
+
+# Run and export plot of regression
+plot_regression_scatterplot(
+  penguins,
+  flipper_length_mm,
+  body_mass_g,
+  output_model_figure
+)
+
+
+
